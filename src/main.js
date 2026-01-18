9bdfaaed3412205e029848b55ea5b904395d905c
juancarlosdhhr <juancarlosdhhr@gmail.com>
2026-01-18 19:21:30 +0000

    main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './styles/base.css';
+
+import Header from './components/Header.js';
+import Hero from './components/Hero.js';
+import Residences from './components/Residences.js';
+import Amenities from './components/Amenities.js';
+import Footer from './components/Footer.js';
+
+Header();
+Hero();
+Residences();
+Amenities();
+Footer();
