220f0aef09a791dbc39aea01eb7baeb1efca822f
juancarlosdhhr <juancarlosdhhr@gmail.com>
2026-01-18 20:04:54 +0000

    cambio estructura

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './styles/base.css';
+import './style.css';
 
-import Header from './components/Header.js';
-import Hero from './components/Hero.js';
-import Residences from './components/Residences.js';
-import Amenities from './components/Amenities.js';
-import Footer from './components/Footer.js';
+import Header from './components/Header/Header.js';
+import Hero from './components/Hero/Hero.js';
+import Residences from './components/Residences/Residences.js';
+import Amenities from './components/Amenities/Amenities.js';
+import Footer from './components/Footer/Footer.js';
 
 Header();
 Hero();
